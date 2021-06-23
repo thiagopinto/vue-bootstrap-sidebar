f28c558562038c29c6d69e9748bb9eb48c3c740a
Thiago Pinto Dias <thiagopinto.lx@gmail.com>
2021-06-23 12:45:19 -0300

    Deploy 1

--- src/vue-bootstrap-sidebar.vue
+++ src/vue-bootstrap-sidebar.vue
@@ -3,31 +3,24 @@
     <!-- Sidebar -->
     <nav
       id="sidebar"
-      :class="
-        `vue-bootstrap-sidebar default-theme  ${[theme] && [
-          show ? 'sidebar-visible' : 'sidebar-hidden'
-        ]} ${align}`
-      "
+      :class="`vue-bootstrap-sidebar default-theme  ${
+        [theme] && [show ? 'sidebar-visible' : 'sidebar-hidden']
+      } ${align}`"
     >
       <!-- eslint-disable vue/no-v-html -->
-      <div
-        v-if="header"
-        class="sidebar-header"
-        v-html="header"
-      >
-      </div>
-      <div
-        v-else
-        class="sidebar-header"
-        @click="onButtonClick"
-      >
+      <div v-if="header" class="sidebar-header" v-html="header"></div>
+      <div v-else class="sidebar-header" @click="onButtonClick">
         <slot name="logo-mini" />
       </div>
       <!-- eslint-enable -->
       <b-list-group class="items-wrapper">
         <template v-for="(link, index) in links">
           <template v-if="link.href !== undefined">
-            <b-list-group-item :key="index">
+            <b-list-group-item
+              :key="index"
+              v-b-tooltip.hover.right
+              :title="link.name"
+            >
               <b-button
                 block
                 :to="link.href"
@@ -50,7 +43,11 @@
             </b-list-group-item>
           </template>
           <template v-else>
-            <b-list-group-item :key="index">
+            <b-list-group-item
+              :key="index"
+              v-b-tooltip.hover.right
+              :title="link.name"
+            >
               <b-button
                 v-b-toggle="`accordion-${index + 10}`"
                 block
@@ -106,28 +103,24 @@
           </template>
         </template>
       </b-list-group>
+      <slot name="footer" />
       <!--/ .items-wrapper -->
       <HamburgerButton
         id="sidebarButton"
         class="sidebar-button"
-        :is-hamburger="!initialShow"
+        :is-hamburger="initialShow"
         :class="[show ? 'visible' : 'hidden']"
         @click="onButtonClick"
       />
     </nav>
-    <div
-      id="navbar"
-      :class="`${[show ? 'sidebar' : 'no-sidebar']} ${align}`"
-    >
+    <div id="navbar" :class="`${[show ? 'sidebar' : 'no-sidebar'] } ${align}`">
       <slot name="navbar" />
     </div>
     <!-- Hamburger Menu -->
-    <div
-      id="content"
-      :class="`${[show ? 'sidebar' : 'no-sidebar']} ${align}`"
-    >
+    <div id="content" :class="`${[show ? 'sidebar' : 'no-sidebar'] } ${align}`">
       <slot name="content" />
     </div>
+
   </div>
 </template>
 <script>
@@ -136,41 +129,70 @@ import HamburgerButton from "@jurajkavka/vue-hamburger-button";
 export default {
   name: "BootstrapSidebar",
   components: {
-    HamburgerButton
+    HamburgerButton,
   },
   props: {
     links: {
       type: Array,
-      default: null
+      default: null,
     },
     initialShow: {
       type: Boolean,
-      default: true
+      default: false,
     },
     theme: {
       type: String,
-      default: "default-theme"
+      default: "default-theme",
     },
     header: {
       type: String,
-      default: ""
+      default: "",
     },
     align: {
       type: String,
-      default: "left"
-    }
+      default: "left",
+    },
   },
   data() {
     return {
-      show: this.initialShow
+      show: this.initialShow,
     };
+  },
+  beforeMount() {
+    /*     this.$nextTick(function () {
+      this.onResize();
+    }); */
+
+    window.addEventListener("resize", this.onResize);
+    let width = window.document.documentElement.clientWidth;
+    if (width <= 991.98) {
+      this.show = false;
+    } else {
+      this.show = true;
+    }
+  },
+  beforeDestroy() {
+     window.removeEventListener("resize", this.onResize);
   },
   methods: {
     onButtonClick() {
       this.show = !this.show;
       this.$emit("sidebar-changed", this.show);
-    }
-  }
+    },
+    onResize(windowResize) {
+      let targetElement = windowResize.target || windowResize.srcElement;
+      if (targetElement != undefined) {
+        //(min-width: 576px) and (max-width: 991.98px)
+        console.log(targetElement.document.documentElement.clientWidth);
+        let width = targetElement.document.documentElement.clientWidth;
+        if (width <= 991.98) {
+          this.show = false;
+        } else {
+          this.show = true;
+        }
+      }
+    },
+  },
 };
 </script>
 <style lang="scss">
